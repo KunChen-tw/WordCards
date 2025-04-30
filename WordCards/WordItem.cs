110bc735f0d1eb004ccc107b26565e3faf26783c
user <user@1008-TH>
2025-04-30 16:01:11 +0800

    Add project files.

---
+++ WordCards/WordItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTSVFile
+{
+    class WordItem
+    {
+        #region 成員變數
+        public string Word { get; set; }
+        public string Phonogram { get; set; }
+        public string SoundPath { get; set; }
+        public string Explain { get; set; }
+        #endregion
+
+        public WordItem(string str)
+        {
+            string[] strLists = str.Split('\t');
+            if (strLists.Length < 3)
+            {
+                throw new ArgumentException("Invalid input string format.");
+            }
+            else // list.Length >= 3
+            {
+                Word = strLists[0];
+                Phonogram = strLists[1];
+                SoundPath = strLists[2];
+                Explain = string.Join(Environment.NewLine, strLists.Skip(3)); 
+            }
+        }
+    }
+}
